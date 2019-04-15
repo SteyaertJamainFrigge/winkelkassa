3df508f2efdc1b3bd896723ce346eb2ec61ab2d7
SteyaertJamainFrigge <frigge.steyaert.jamain@student.howest.be>
2019-04-15 22:43:49 +0200

    changed db password

--- src/main/java/FriggeSteyaertJamain/be/winkelKassa/data/MySqlConnection.java
+++ src/main/java/FriggeSteyaertJamain/be/winkelKassa/data/MySqlConnection.java
@@ -7,16 +7,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class MySqlConnection {
+class MySqlConnection {
 
     private static final String URL = "jdbc:mysql://localhost/kassa";
     private static final String UID = "kassa_usr";
-    private static final String PWD = "kassa_pwd";
+    private static final String PWD = "Kassa1_pwd*";
 
-    public static Connection getConnection(){
+    static Connection getConnection(){
         try {
-            Connection con = DriverManager.getConnection(URL, UID, PWD);
-            return con;
+            return DriverManager.getConnection(URL, UID, PWD);
         } catch (SQLException e) {
             throw new KassaException("Cannot connect to DB", e);
         }
