e19d739b4a5ed15966abb276f65a77ff0128591f
SteyaertJamainFrigge <frigge.steyaert.jamain@student.howest.be>
2019-06-15 18:45:36 +0200

    code cleanup

--- src/main/java/FriggeSteyaertJamain/be/winkelKassa/ui/gui/BarcodePrinterController.java
+++ src/main/java/FriggeSteyaertJamain/be/winkelKassa/ui/gui/BarcodePrinterController.java
@@ -6,34 +6,43 @@ import FriggeSteyaertJamain.be.winkelKassa.util.KassaException;
 import com.itextpdf.text.DocumentException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
 import FriggeSteyaertJamain.be.winkelKassa.domain.barcode.BarCode128Generator;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import java.awt.*;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class BarcodePrinterController extends SubWindow {
 
     @FXML
     private Spinner<Integer> amountSpinner;
 
-    private String barcode;
+    @FXML
+    private Label productName;
+
+    @FXML
+    private Label barcodeValue;
+
+    @FXML
+    private Button returnBtn;
 
     private int productid;
-
-    public void setBarcode(String barcode) {
-        this.barcode = barcode;
-    }
-
-    public void setProductid(int productid) {
-        this.productid = productid;
-    }
 
     public void initialize(){
         changeReturnBtnStyle();
         SpinnerValueFactory<Integer> spinnerValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0,200, 1);
         this.amountSpinner.setValueFactory(spinnerValueFactory);
+    }
+
+    void iniData(Product product){
+        this.productName.setText(product.getName());
+        this.barcodeValue.setText(product.getBarcode());
+        this.productid = product.getId();
     }
 
     private boolean validateAmountInput() {
@@ -48,34 +57,37 @@ public class BarcodePrinterController extends SubWindow {
     private void showErrorPane(String input) {
         Alert al = new Alert(Alert.AlertType.ERROR);
         al.setContentText(input);
-        DialogPane dialogPane = al.getDialogPane();
-        dialogPane.getStylesheets().add("/style/css/flatterfx.css");
         al.showAndWait();
     }
 
-    private void printBarcode() {
+    private void generateBarcode() {
         try {
             int amount = this.amountSpinner.getValue();
-            BarCode128Generator bcg = new BarCode128Generator("A4", barcode, amount);
+            BarCode128Generator bcg = new BarCode128Generator("A4", this.barcodeValue.getText(), amount);
             bcg.run();
         } catch (FileNotFoundException e) {
+            showErrorPane("De pdf is in gebruik door een ander process");
             throw new KassaException("Unable to Find pdf File", e);
         } catch (DocumentException e) {
             throw new KassaException("An error occurred with the Document", e);
         }
     }
 
-    @FXML
-    void confirm(ActionEvent event) {
-        if (!validateProductInput()){
-            showErrorPane("product bestaat niet");
-        }else {
-            if (!validateAmountInput()) {
-                showErrorPane("aantal niet aanvaard input.");
-            } else {
-                printBarcode();
+    private void openPDF(){
+        if (Desktop.isDesktopSupported()) {
+            try {
+                File myFile = new File("temp/pdf/bar128.pdf");
+                Desktop.getDesktop().open(myFile);
+            } catch (IOException ex) {
+                throw new KassaException("can't open pdf", ex);
             }
         }
+    }
+
+    @FXML
+    void confirm(ActionEvent event) {
+        generateBarcode();
+        openPDF();
     }
 
     @FXML
