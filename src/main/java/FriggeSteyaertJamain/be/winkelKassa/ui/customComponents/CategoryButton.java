0f82a2548c9c71f2bc68399856f7de40ecc30ebf
SteyaertJamainFrigge <frigge.steyaert.jamain@student.howest.be>
2019-09-08 20:38:43 +0200

    created constructor without parameter so CategoryButton can now be used in fxml file

--- src/main/java/FriggeSteyaertJamain/be/winkelKassa/ui/customComponents/CategoryButton.java
+++ src/main/java/FriggeSteyaertJamain/be/winkelKassa/ui/customComponents/CategoryButton.java
@@ -3,23 +3,41 @@ package FriggeSteyaertJamain.be.winkelKassa.ui.customComponents;
 import FriggeSteyaertJamain.be.winkelKassa.domain.register.Product;
 import FriggeSteyaertJamain.be.winkelKassa.domain.register.ProductCategory;
 import javafx.scene.control.Button;
-
+import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
-public class CategoryButton extends Button {
+public class CategoryButton extends Button{
 
     private ProductCategory category;
 
-    public CategoryButton(ProductCategory category) {
+    public CategoryButton(){
+        super();
+        this.category = null;
+        this.setDisable(true);
+    }
+
+    public CategoryButton(@NotNull ProductCategory category) {
         super(category.getName());
         this.category = category;
     }
 
     public List<ProductCategory> getSubCategories(){
-        return category.getSubCategories();
+        return this.category.getSubCategories();
     }
 
     public List<Product> getProducts(){
         return category.getProducts();
+    }
+
+    public ProductCategory getCategory() {
+        return this.category;
+    }
+    public String getName(){
+        return this.category.getName();
+    }
+
+    public void setCategory(ProductCategory category) {
+        this.category = category;
+        this.setDisable(false);
     }
 }
