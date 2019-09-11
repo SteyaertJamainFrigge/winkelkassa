27a4fd886055092fbfbf01d499f57e3ed191972c
SteyaertJamainFrigge <frigge.steyaert.jamain@student.howest.be>
2019-09-11 18:43:48 +0200

    created VoorbetalingController

---
+++ src/main/java/FriggeSteyaertJamain/be/winkelKassa/ui/gui/VoorbetalingController.java
@@ -0,0 +1,11 @@
+package FriggeSteyaertJamain.be.winkelKassa.ui.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class VoorbetalingController extends SubWindow{
+
+}
