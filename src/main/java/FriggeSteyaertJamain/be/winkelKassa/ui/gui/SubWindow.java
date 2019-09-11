b288c97c6c9fb733c98cbaa5ff7295d81c8072e6
SteyaertJamainFrigge <frigge.steyaert.jamain@student.howest.be>
2019-09-11 18:42:19 +0200

    supWindows can now just close

--- src/main/java/FriggeSteyaertJamain/be/winkelKassa/ui/gui/SubWindow.java
+++ src/main/java/FriggeSteyaertJamain/be/winkelKassa/ui/gui/SubWindow.java
@@ -1,28 +1,41 @@
 package FriggeSteyaertJamain.be.winkelKassa.ui.gui;
 
+import FriggeSteyaertJamain.be.winkelKassa.util.KassaException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.Region;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 abstract class SubWindow {
 
     @FXML
-    protected Button returnBtn;
+    AnchorPane root;
 
-    void returnToMainScene() throws Exception{
-        Stage stage = (Stage) returnBtn.getScene().getWindow();
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/start.fxml"));
-        Scene scene = new Scene(root);
-        stage.hide();
-        stage.setScene(scene);
-        stage.setMaximized(false);
-        stage.setFullScreen(false);
-        stage.centerOnScreen();
-        stage.show();
+    @FXML
+    private void returnToMainScene(){
+        Stage stage = (Stage) root.getScene().getWindow();
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/start.fxml"));
+            Scene scene = new Scene(root);
+            stage.hide();
+            stage.setScene(scene);
+            stage.setMaximized(false);
+            stage.setFullScreen(false);
+            stage.centerOnScreen();
+            stage.show();
+        }catch (IOException e){
+            throw new KassaException("Unable to return to main scene");
+        }
+    }
+
+    @FXML
+    private void closeWindow(){
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.close();
     }
 
 }
