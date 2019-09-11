f954c247250275bad7713071527a538938aee856
SteyaertJamainFrigge <frigge.steyaert.jamain@student.howest.be>
2019-09-11 18:38:00 +0200

    updated edit and save functionality

--- src/main/java/FriggeSteyaertJamain/be/winkelKassa/ui/gui/ArticleManagementController.java
+++ src/main/java/FriggeSteyaertJamain/be/winkelKassa/ui/gui/ArticleManagementController.java
@@ -6,6 +6,8 @@ import FriggeSteyaertJamain.be.winkelKassa.domain.register.Product;
 import FriggeSteyaertJamain.be.winkelKassa.domain.register.ProductCategory;
 import FriggeSteyaertJamain.be.winkelKassa.util.KassaException;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,24 +17,34 @@ import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ArticleManagementController extends SubWindow {
 
-    public TextField locationInput;
-    public TextArea descriptionInput;
-    public Button PhotoBtn;
-    public Button zoomInBtn;
-    public ImageView articlePreviewImage;
-    public Button databaseBtn;
-    public TextField filterInput;
-    public Button optionsBtn;
-    public Button filterBtn;
-    public Button okBtn;
-    public Button plusBtn;
-    private int id;
+    @FXML
+    private Button editBtn;
+    @FXML
+    private TextField locationInput;
+    @FXML
+    private TextArea descriptionInput;
+    @FXML
+    private Button PhotoBtn;
+    @FXML
+    private Button zoomInBtn;
+    @FXML
+    private ImageView articlePreviewImage;
+    @FXML
+    private Button databaseBtn;
+    @FXML
+    private TextField filterInput;
+    @FXML
+    private Button okBtn;
+    @FXML
+    private Button plusBtn;
+    @FXML
     private List<ProductCategory> categories;
     @FXML
     private ListView<Product> productList;
@@ -48,17 +60,6 @@ public class ArticleManagementController extends SubWindow {
     private ComboBox<ProductCategory> categoryComboBx;
     @FXML
     private ComboBox<Btw> btwComboBx;
-    @FXML
-    private GridPane productInputs;
-
-    @FXML
-    void returnToMain() {
-        try {
-            returnToMainScene();
-        } catch (Exception e) {
-            throw new KassaException("Unable to go back to main scene", e);
-        }
-    }
 
     public void initialize() {
         initializeSpinner();
@@ -67,37 +68,49 @@ public class ArticleManagementController extends SubWindow {
         fillBtw();
         addProductListListener();
         this.productList.getSelectionModel().selectFirst();
+        enableDisableInputFields(false);
     }
 
-    private void initializeSpinner(){
-        SpinnerValueFactory<Double> spinnerValueFactory = new SpinnerValueFactory.DoubleSpinnerValueFactory(0.00,2000.00);
+    private void initializeSpinner() {
+        SpinnerValueFactory<Double> spinnerValueFactory = new SpinnerValueFactory.DoubleSpinnerValueFactory(0.00, 2000.00);
         this.priceSpinner.setValueFactory(spinnerValueFactory);
     }
 
-    private void addProductListListener(){
+    private void addProductListListener() {
         this.productList.getSelectionModel().selectedItemProperty()
                 .addListener((observable, oldValue, newValue) -> fillProductValues(newValue));
     }
 
-    private void fillProductList(){
+    private void fillProductList() {
         List<Product> products = Repositories.getInstance().getProductRepository().getAllProducts();
-        this.productList.setItems(FXCollections.observableList(products));
+        ObservableList<Product> data = FXCollections.observableArrayList();
+        data.addAll(products);
+        FilteredList<Product> productFilteredList = new FilteredList<>(data, s -> true);
+        filterInput.textProperty().addListener(obs -> {
+            String filter = filterInput.getText();
+            if (filter == null || filter.length() == 0) {
+                productFilteredList.setPredicate(s -> true);
+            } else {
+                productFilteredList.setPredicate(s -> s.getName().contains(filter));
+            }
+        });
+        this.productList.setItems(productFilteredList);
     }
 
-    private void fillBtw(){
+    private void fillBtw() {
         List<Btw> btwTarifs = Repositories.getInstance().getBtwRepository().getAllBtw();
         this.btwComboBx.setItems(FXCollections.observableList(btwTarifs));
     }
 
-    private void fillCategories(){
+    private void fillCategories() {
         List<ProductCategory> categories = Repositories.getInstance().getCategoryRepository().getAllCategories();
         this.categories = categories;
         this.categoryComboBx.setItems(FXCollections.observableList(categories));
     }
 
-    private void fillProductValues(Product product){
+    private void fillProductValues(Product product) {
         this.nameInput.setText(product.getName());
-        //this.priceSpinner.getValueFactory().setValue(product.getPrice());
+        this.priceSpinner.getValueFactory().setValue(product.getPrice());
         this.descriptionInput.clear();
         this.descriptionInput.appendText(product.getDescription());
         this.locationInput.setText(product.getLocation());
@@ -106,22 +119,21 @@ public class ArticleManagementController extends SubWindow {
         ProductCategory pc = findCategoryById(product.getCategory());
         this.categoryComboBx.setValue(pc);
         this.btwComboBx.setValue(product.getBtw());
-        this.id = product.getId();
+        int id = product.getId();
     }
 
     private ProductCategory findCategoryById(int id) {
         ProductCategory category = null;
-        for (ProductCategory item:
+        for (ProductCategory item :
                 categories) {
-            if(item.getId() == id){
+            if (item.getId() == id) {
                 category = item;
             }
         }
         return category;
     }
 
-
-    private void saveChangesToObject(Product product){
+    private void saveChangesToObject(Product product) {
         product.setName(this.nameInput.getText());
         product.setPrice(this.priceSpinner.getValue());
         product.setBarcode(this.barcodeInput.getText());
@@ -132,15 +144,16 @@ public class ArticleManagementController extends SubWindow {
         product.setBtw(this.btwComboBx.getValue());
     }
 
-    private void savechangedObjectToDB(Product productToSave){
+    private void savechangedObjectToDB(Product productToSave) {
         Repositories.getInstance().getProductRepository().updateProduct(productToSave);
     }
 
     @FXML
-    private void save(){
+    private void save() {
         Product productToSave = this.productList.getSelectionModel().getSelectedItem();
         saveChangesToObject(productToSave);
         savechangedObjectToDB(productToSave);
+        enableDisableInputFields(false);
     }
 
     @FXML
@@ -156,6 +169,27 @@ public class ArticleManagementController extends SubWindow {
         window.setMaximized(false);
         window.show();
     }
+
+    @FXML
+    private void enableInputFields() {
+        enableDisableInputFields(true);
+    }
+
+    private void enableDisableInputFields(boolean bool) {
+        TextInputControl[] inputFields = {
+                this.nameInput,
+                this.barcodeInput,
+                this.descriptionInput,
+                this.locationInput,
+                this.storeInput};
+        for (TextInputControl inputField : inputFields) {
+            inputField.setDisable(!bool);
+        }
+        this.priceSpinner.setDisable(!bool);
+        this.btwComboBx.setDisable(!bool);
+        this.categoryComboBx.setDisable(!bool);
+    }
+
 
 }
 
